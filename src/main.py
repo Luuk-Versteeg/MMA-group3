8ef13c9211a1cdbd2b32ccc54b8aa9ee32645003
Luuk Versteeg <luukversteeg@live.nl>
2024-06-10 12:56:23 +0200

    created new dash project in src folder and added drawio file

--- src/main.py
+++ src/main.py
@@ -1,78 +1,53 @@
-from dash import Dash, html, dcc
-from src import config
-from src.Dataset import Dataset
-from src.widgets import projection_radio_buttons, gallery, scatterplot, wordcloud, graph, heatmap, histogram, help_popup
-from src.widgets.table import create_table
+from dash import Dash, html, dcc, callback, Output, Input
+import plotly.express as px
 import dash_bootstrap_components as dbc
+import pandas as pd
 
-import callbacks.table
-import callbacks.scatterplot
-import callbacks.projection_radio_buttons
-import callbacks.heatmap
-import callbacks.wordcloud
-import callbacks.histogram
-import callbacks.gallery
-import callbacks.deselect_button
-import callbacks.help_button
-import callbacks.graph
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
 
-def run_ui():
-    external_stylesheets = [dbc.themes.BOOTSTRAP]
-    app = Dash(__name__, external_stylesheets=external_stylesheets)
+app = Dash(__name__)
 
-    help_popup_widget = help_popup.create_help_popup()
-    projection_radio_buttons_widget = projection_radio_buttons.create_projection_radio_buttons()
-    table_widget = create_table()
-    scatterplot_widget = scatterplot.create_scatterplot(config.DEFAULT_PROJECTION)
-    wordcloud_widget = wordcloud.create_wordcloud()
-    gallery_widget = gallery.create_gallery()
-    graph_widget = graph.create_graph()
-    heatmap_widget = heatmap.create_heatmap()
-    histogram_widget = histogram.create_histogram()
-
-    right_tab = dcc.Tabs([
-        dcc.Tab(label='wordcloud', children=wordcloud_widget),
-        dcc.Tab(label='sample images', children=gallery_widget),
-        dcc.Tab(label='histogram', children=histogram_widget),
-        dcc.Tab(label='graph', children=graph_widget),
-        dcc.Tab(label='heatmap', children=[heatmap_widget])
+app.layout = dbc.Container([
+    html.Div(children=[
+        html.H1(children='First page', style={'textAlign':'center'}),
+        dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection1'),
+        dcc.Graph(id='graph-content1')
+    ]),
+    html.Div(children=[
+        html.H1(children='Second page', style={'textAlign':'center'}),
+        dcc.Dropdown(df.country.unique(), 'Italy', id='dropdown-selection2'),
+        dcc.Graph(id='graph-content2')
+    ]),
+    html.Div(children=[
+        html.H1(children='Third page', style={'textAlign':'center'}),
+        dcc.Dropdown(df.country.unique(), 'Spain', id='dropdown-selection3'),
+        dcc.Graph(id='graph-content3')
     ])
+], id="carousel")
 
-    app.layout = dbc.Container([
-        help_popup_widget,
-        dbc.Stack([
-            projection_radio_buttons_widget,
-            dbc.Button('Deselect everything', id='deselect-button', class_name="btn btn-outline-primary ms-auto header-button"),
-            dbc.Button('Help', id='help-button', class_name="btn btn-outline-primary header-button")
-        ], id='header', direction="horizontal"),
-        dbc.Row([
-            dbc.Col(scatterplot_widget, width=6, className='main-col'),
-            dbc.Col(right_tab, width=6, className='main-col')],
-            className='top-row', justify='between'),
-        dbc.Row([
-            dbc.Col(table_widget, className='main-col')
-        ], className='bottom-row')
-    ], fluid=True, id='container')
+@callback(
+    Output('graph-content1', 'figure'),
+    Input('dropdown-selection1', 'value')
+)
+def update_graph(value):
+    dff = df[df.country==value]
+    return px.line(dff, x='year', y='pop')
 
-    app.run(debug=True, use_reloader=False)
+@callback(
+    Output('graph-content2', 'figure'),
+    Input('dropdown-selection2', 'value')
+)
+def update_graph(value):
+    dff = df[df.country==value]
+    return px.line(dff, x='year', y='pop')
 
-
-def main():
-    if not Dataset.files_exist():
-        print('File', config.AUGMENTED_DATASET_PATH, 'missing or file', config.ATTRIBUTE_DATA_PATH, 'missing or directory', config.IMAGES_DIR, 'missing')
-        print('Creating dataset.')
-        Dataset.download()
-
-    Dataset.load()
-
-    if len(Dataset.get()) != config.DATASET_SAMPLE_SIZE:
-        print('Sample size changed in the configuration. Recalculating features.')
-        Dataset.download()
-        Dataset.load()
-
-    print('Starting Dash')
-    run_ui()
-
+@callback(
+    Output('graph-content3', 'figure'),
+    Input('dropdown-selection3', 'value')
+)
+def update_graph(value):
+    dff = df[df.country==value]
+    return px.line(dff, x='year', y='pop')
 
 if __name__ == '__main__':
-    main()
+    app.run(debug=True)
