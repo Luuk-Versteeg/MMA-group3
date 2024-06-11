14af6d604b12ad21881da5cd4e959cee51172b23
Tobie1999 <tobiewerner@gmail.com>
2024-06-11 19:20:23 +0200

    Added variables & variant menus with callbacks

--- src/main.py
+++ src/main.py
@@ -1,11 +1,15 @@
-from dash import Dash, html, dcc, callback, Output, Input
+from dash import Dash, html, dcc, Output, Input, State, ctx, callback
 import plotly.express as px
 import dash_bootstrap_components as dbc
 import pandas as pd
+from collections import defaultdict
+
 
 df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
 
 app = Dash(__name__)
+
+all_variants = defaultdict(dict)
 
 app.layout = dbc.Container([
     html.Div(children=[
@@ -19,9 +23,9 @@ app.layout = dbc.Container([
             html.Div(
                 className='box',
                 children=[
-                    html.Button('<', id='button-generate', style={'height':'100%', 'width':50, 'display':'inline-block'}),
+                    html.Button('<', id='button-left', style={'height':'100%', 'width':50, 'display':'inline-block'}),
                     html.Div(style={'height':'100%', 'flex':1, 'text-align':'center'}, children='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'),
-                    html.Button('>', id='button-test', style={'height':'100%', 'width':50, 'display':'inline-block'})
+                    html.Button('>', id='button-right', style={'height':'100%', 'width':50, 'display':'inline-block'})
                 ],
                 style={'width':'50%', 'height':100, 'display':'flex'},
             ),
@@ -33,62 +37,58 @@ app.layout = dbc.Container([
         style={'display':'flex', 'justify-content':'space-between'}
         ),
         html.Div(children=[
-            dcc.Textarea(
-                id='textarea-example',
-                value='{q2}{text}\n\n{q1}',
-                style={'width':'58%', 'height':50, 'display':'inline-block', 
-                       'resize':'vertical'},
-            ),
-            dcc.Input(
-                id='input-example',
-                value='Business, World, Science, Sports',
-                type='text',
-                style={'width':'38%', 'height': 50, 'display': 'inline-block'},
-            )], 
+                dcc.Textarea(
+                    id='textarea-example',
+                    value='{q2}{text}\n\n{q1}',
+                    style={'width':'58%', 'height':50, 'display':'inline-block', 
+                        'resize':'vertical'},
+                ),
+                dcc.Input(
+                    id='input-test1',
+                    value='Business, World, Science, Sports',
+                    type='text',
+                    style={'width':'38%', 'height': 50, 'display': 'inline-block'},
+                )
+            ], 
             style={'display':'flex', 'justify-content':'space-between'}
         ),
         html.Div(children=[
             html.Div(
                 className='box',
                 children=[
-                    dcc.Tabs(id="tabs", value='var1', children=[
-                        dcc.Tab(label='var1', value='var1'),
-                        dcc.Tab(label='var2', value='var2'),
-                        dcc.Tab(label='var3', value='var3'),
+                    html.Div(className='box', children=[
+                        html.Button('Add variable', id='add-variable-button', n_clicks=0),
+                        html.Button('Remove selected variable', id='remove-variable-button')
                     ]),
-                    html.Div(id='tabs-content')
+                    dcc.Tabs(content_style={"width": "100%"},
+                             parent_style={"width": "100%"},
+                             style={'width':'100%'},
+                             id="variable-container", children=[], vertical=True),
+
                 ],
-                style={'width':'47%', 'height':400, 'display':'inline-block'},
+                style={'width':'47%', 'height':400},
             ),
             html.Div(
                 className='box',
                 children=[
                     html.Div(
                         id='tabs-content',
-                        children='varX',
+                        children=[ 
+                                  html.Button('Add variant', id='add-variant-button', n_clicks=0),
+                                  ],
                         style={'display':'flex',
                                'justify-content':'center',
                                'align-items':'center', 
                                'height':50}
                     ),
-                    dcc.Input(
-                        id='input-example',
-                        value='A',
-                        type='text',
-                        style={'width':'100%', 'height': 35},
-                    ), 
-                    dcc.Input(
-                        id='input-example',
-                        value='B',
-                        type='text',
-                        style={'width':'100%', 'height': 35},
-                    ),
-                    dcc.Input(
-                        id='input-example',
-                        value='C',
-                        type='text',
-                        style={'width':'100%', 'height': 35},
-                    )
+                    html.Div(id='variant-container', children=[
+                        # dcc.Input(
+                        #     id='input-test2',
+                        #     value='variant',
+                        #     type='text',
+                        #     style={'width':'100%', 'height': 35},
+                        # )
+                    ]),
                 ],
                 style={'width':'47%', 'height':400, 'display':'inline-block'},
             )
@@ -100,7 +100,7 @@ app.layout = dbc.Container([
                 html.Button('Test prompts', id='button-test')
             ], 
             style={'width':'100%'}
-        )
+        ),
     ]),
     html.Div(children=[
         html.H1(children='Third page', style={'textAlign':'center'}),
@@ -117,13 +117,81 @@ def update_graph(value):
     dff = df[df.country==value]
     return px.line(dff, x='year', y='pop')
 
-# @callback(
-#     Output('graph-content2', 'figure'),
-#     Input('dropdown-selection2', 'value')
-# )
-# def update_graph(value):
-#     dff = df[df.country==value]
-#     return px.line(dff, x='year', y='pop')
+@callback(
+    Output('variable-container', 'children'),
+    Output('variable-container', 'value'),
+    Input('add-variable-button', 'n_clicks'),
+    Input('remove-variable-button', 'n_clicks'),
+    State('variable-container', 'children'),
+    State('variable-container', 'value')
+)
+def update_tabs(add_clicks, remove_clicks, tabs, active_tab):
+    ctx_id = ctx.triggered_id
+
+    if ctx_id == 'add-variable-button':
+        new_index = len(tabs) + 1 
+        new_tab_value = f'tab-{new_index}'
+        new_tab = dcc.Tab(label=f'Variable {new_index}', value=new_tab_value, 
+                          id={'type': 'tab', 'index': new_index},
+                          style={'width':'100%', 'line-width': '100%'},
+                          selected_style={'width':'100%', 'line-width': '100%'})
+        tabs.append(new_tab)
+        active_tab = new_tab_value
+
+    elif ctx_id == 'remove-variable-button' and active_tab is not None:
+        if active_tab in all_variants:
+            #all_variants.pop(active_tab, None)
+            all_variants[active_tab] = {}
+
+        tabs = [tab for tab in tabs if tab['props']['value'] != active_tab]
+        if tabs:
+            active_tab = tabs[0]['props']['value']
+        else:
+            active_tab = None
+
+    return tabs, active_tab
+
+
+@callback(
+    Output('variant-container', 'children'),
+    Input('add-variant-button', 'n_clicks'),
+    Input('variable-container', 'value'),
+    State('variant-container', 'children'),
+    State('variable-container', 'value')
+)
+def update_variants(add_clicks, pressed_tab, variants, tabs_state):
+    ctx_id = ctx.triggered_id
+
+    if ctx_id == 'add-variant-button':
+        if tabs_state == None:
+            return variants
+
+        idx = len(variants) + 1
+        all_variants[tabs_state][idx] = f"Variant {idx}"
+
+        new_variant = html.Div(style={'display':'grid', 'grid-template-columns':'80% 20%','height':40}, children=[
+            dcc.Input(id='input-variant-' + str(idx),
+                    value=all_variants[tabs_state][idx],
+                    type='text'),
+            html.Button('X', id='button-delete'),
+        ])
+        variants.append(new_variant)
+
+    elif ctx_id == 'variable-container':
+        if pressed_tab in all_variants:
+            vars = [html.Div(style={'display':'grid', 'grid-template-columns':'80% 20%','height':40}, children=[
+                dcc.Input(id='input-variant-' + str(idx),
+                        value=val,
+                        type='text'),
+                html.Button('X', id='button-delete'),
+            ]) for idx, val in all_variants[pressed_tab].items()]
+
+            variants = vars
+        else:
+            variants = []
+
+    return variants
+
 
 @callback(
     Output('graph-content3', 'figure'),
@@ -132,20 +200,6 @@ def update_graph(value):
 def update_graph(value):
     dff = df[df.country==value]
     return px.line(dff, x='year', y='pop')
-
-@callback(Output('tabs-content', 'children'),
-              Input('tabs', 'value'))
-def render_content(tab):
-    print(tab)
-    if tab == 'var1':
-        return html.Div([
-            html.H3('Tab content 1')
-        ])
-    elif tab == 'var2':
-        return html.Div([
-            html.H3('Tab content 2')
-        ])
-
 
 
 if __name__ == '__main__':
